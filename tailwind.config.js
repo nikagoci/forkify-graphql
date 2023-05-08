77cf2a4802701f9ad823a2e1e8dd017e4b85b23b
nikagoci <86802785+nikagoci@users.noreply.github.com>
2023-05-08 16:03:36 +0400

    added local images/style

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,11 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        orange: '#ff9319',
+        red_orange: '#f38e82',
+        black: '#655A56'
+      }
     },
   },
   plugins: [],
